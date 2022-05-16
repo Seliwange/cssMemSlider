915b13de2f1cadc91582e6d811772f70ab81f92d
Seliwange <yul.yaremchuk@gmail.com>
2022-05-16 22:05:26 +0300

    feat: add desktop version

---
+++ cssMemSlider/js/script.js
@@ -0,0 +1,42 @@
+let slideIndex = 0;
+let timeoutId;
+
+function showSlides(index) {
+    clearTimeout(timeoutId);
+    const slides = document.querySelectorAll(".slider__items");
+    const dots = document.querySelectorAll(".dot");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" dot--active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " dot--active";
+
+    dots.forEach((dot, index) => {
+        dot.addEventListener("click", () => {
+            slides.forEach(slide => {
+                slide.style.display = "none";
+            });
+            for (let i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" dot--active", "");
+            }
+            slides[index].style.display = "block";
+            dots[index].className += " dot--active";
+        });
+    });
+
+    autoLoop();
+}
+
+function autoLoop(){
+    timeoutId = setTimeout(() => {
+        showSlides();
+    }, 5000);
+}
+
+showSlides();
+autoLoop();
